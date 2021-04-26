9ec630989338e18ddb21c3e56e730f617ad0edda
Gyuris Csaba <gyuris.csaba@vasvari.hu>
2021-04-26 10:50:47 +0200

    Add files via upload

---
+++ model/WaterTemperatureException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace oop.model
+{
+    [Serializable]
+    internal class WaterTemperatureException : Exception
+    {
+        public WaterTemperatureException()
+        {
+        }
+
+        public WaterTemperatureException(string message) : base(message)
+        {
+        }
+
+        public WaterTemperatureException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected WaterTemperatureException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
